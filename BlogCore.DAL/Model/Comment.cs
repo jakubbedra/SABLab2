798c8ce57493e82a774cecb2e7579e5953cfee7c
jakubbedra <jakubbedra@wp.pl>
2024-03-23 23:28:19 +0100

    comments

--- BlogCore.DAL/Model/Comment.cs
+++ BlogCore.DAL/Model/Comment.cs
@@ -5,12 +5,17 @@ namespace Blog.DAL.Model;
 
 public class Comment
 {
+    // auto-generated id
     [Key]
     public long Id { get; set; }
 
+    // content of the comment
     public string Content { get; set; }
     
+    // the id of the commented post
     [ForeignKey("Post")]
     public long PostId { get; set; }
+    
+    // commented post
     public Post Post { get; set; }
 }

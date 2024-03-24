4eb205cebf23ca10c48b09fe8a604430016ea530
jakubbedra <jakubbedra@wp.pl>
2024-03-24 13:53:15 +0100

    ..

--- BlogCore.DAL/Model/Author.cs
+++ BlogCore.DAL/Model/Author.cs
@@ -5,6 +5,5 @@ public class Author
     public long Id { get; set; }
     public string Name { get; set; }
     public  string Ip { get; set; }
-    public string Address { get; set; }
     public bool Gender { get; set; }
 }

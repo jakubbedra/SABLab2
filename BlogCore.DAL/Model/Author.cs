ad5cb87638aa3c994a78ff6ee34ac784f32b100e
jakubbedra <jakubbedra@wp.pl>
2024-03-24 11:09:33 +0100

    added credit card entity

--- BlogCore.DAL/Model/Author.cs
+++ BlogCore.DAL/Model/Author.cs
@@ -4,5 +4,4 @@ public class Author
 {
     public string Name { get; set; }
     public string Description { get; set; }
-    public long CreditCardNumber { get; set; }
 }
---
+++ BlogCore.DAL/Model/CreditCard.cs
@@ -0,0 +1,8 @@
+﻿namespace Blog.DAL.Model;
+
+public class CreditCard
+{
+    public long Number {get; set; }
+    public string ExpirationDate { get; set; }
+    public int SecurityCode { get; set; }
+}
